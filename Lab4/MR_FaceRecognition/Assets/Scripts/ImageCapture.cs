b22f1142e5c6f882891b28fa51d4e9f610b71db4
fred le <flecoqui@hotmail.com>
2018-02-11 22:32:15 +0100

    Add Lab 4 Face recognition

---
+++ Lab4/MR_FaceRecognition/Assets/Scripts/ImageCapture.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.XR.WSA.WebCam;
+
+public class ImageCapture : MonoBehaviour {
+
+    public static ImageCapture instance;   // static instance of this class           
+    public int tapsCount; // counter for tap input     
+    PhotoCapture photoCaptureObject = null; // object resulting from photo capture     
+    GestureRecognizer recognizer; // Hololens input recognizer 
+
+    private void Awake()
+    {   // allows this instance to behave like a singleton      
+        instance = this;
+    } 
+
+    void Start()
+    {  // subscribing to the Hololens API gesture recognizer to track user gestures         
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.Tapped += TapHandler;
+        recognizer.StartCapturingGestures();
+    }
+    /// <summary>     
+    /// 
+    /// Respond to Tap Input.     
+    /// </summary>     
+    private void TapHandler(TappedEventArgs obj)
+    {         // increment taps count, used to name images when saving        
+        tapsCount++;
+        // Begins the image capture and analysis procedure         
+
+        ExecuteImageCaptureAndAnalysis();
+    }
+
+
+    void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Call StopPhotoMode once the image has successfully captured
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    } 
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Dispose from the object in memory and request the image analysis          
+        // to the Oxford class 
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+        StartCoroutine(Oxford.instance.DetectFaces());
+    }
+
+
+    private void ExecuteImageCaptureAndAnalysis()
+    {
+        // Set the camera resolution to be the highest possible
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        Texture2D targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Begin capture process, set the image format
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
+            photoCaptureObject = captureObject; 
+
+            CameraParameters c = new CameraParameters();
+            c.hologramOpacity = 0.0f;
+            c.cameraResolutionWidth = targetTexture.width;
+            c.cameraResolutionHeight = targetTexture.height;
+            c.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Capture the image from the camera and save it in the App internal folder
+            captureObject.StartPhotoModeAsync(c, delegate (PhotoCapture.PhotoCaptureResult result)
+            {
+                string filename = string.Format(@"CapturedImage{0}.jpg", tapsCount);
+                string filePath = Path.Combine(Application.persistentDataPath, filename);
+                Oxford.instance.imagePath = filePath;
+                photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
+
+            });
+        });
+    }
+
+
+
+
+
+
+            // Update is called once per frame
+            void Update () {
+		
+	}
+}
---
+++ Lab4/PersonMaker/PersonMaker/MainPage.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Storage;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Windows.System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+
+namespace PersonMaker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string authKey;
+        string personGroupId;
+        string personGroupName;
+
+        Guid personId;
+        string personName;
+        StorageFolder personFolder;
+
+        private FaceServiceClient faceServiceClient;
+        private PersonGroup knownGroup;
+        private int minPhotos = 6;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            personName = string.Empty;
+            authKey = string.Empty;
+            personGroupId = string.Empty;
+            personGroupName = string.Empty;
+            personId = Guid.Empty;
+        }
+
+        /// <summary>
+        /// Create a person group with ID and name provided if none can be found in the service.
+        /// </summary>
+        private async void CreatePersonGroupButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            personGroupId = PersonGroupIdTextBox.Text;
+            personGroupName = PersonGroupNameTextBox.Text;
+            PersonGroupStatusTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            authKey = AuthKeyTextBox.Text;
+            faceServiceClient = new FaceServiceClient(authKey);
+
+            if (null != faceServiceClient)
+            {
+                // You may experience issues with this below call, if you are attempting connection with
+                // a service location other than 'West US'
+                PersonGroup[] groups = await faceServiceClient.ListPersonGroupsAsync();
+                var matchedGroups = groups.Where(p => p.PersonGroupId == personGroupId);
+
+                if (matchedGroups.Count() > 0)
+                {
+                    knownGroup = matchedGroups.FirstOrDefault();
+
+                    PersonGroupStatusTextBlock.Text = "Found existing: " + knownGroup.Name;
+                }
+
+                if (null == knownGroup)
+                {
+                    await faceServiceClient.CreatePersonGroupAsync(personGroupId, personGroupName);
+                    knownGroup = await faceServiceClient.GetPersonGroupAsync(personGroupId);
+
+                    PersonGroupStatusTextBlock.Text = "Created new group: " + knownGroup.Name;
+                }
+
+                if (PersonGroupStatusTextBlock.Text != "- Person Group status -")
+                {
+                    PersonGroupStatusTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    PersonGroupStatusTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+        }
+
+        private async void CreatePersonButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            personName = PersonNameTextBox.Text;
+            PersonStatusTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            if (knownGroup != null && personName.Length > 0)
+            {
+                //Check if this person already exist
+                bool personAlreadyExist = false;
+                Person[] ppl = await GetKnownPeople();
+                foreach (Person p in ppl)
+                {
+                    if (p.Name == personName)
+                    {
+                        personAlreadyExist = true;
+                        PersonStatusTextBlock.Text = $"Person already exist: {p.Name} ID: {p.PersonId}";
+
+                        PersonStatusTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                    }
+                }
+
+                if (!personAlreadyExist)
+                {
+                    CreatePersonResult result = await faceServiceClient.CreatePersonAsync(personGroupId, personName);
+                    if (null != result && null != result.PersonId)
+                    {
+                        personId = result.PersonId;
+
+                        PersonStatusTextBlock.Text = "Created new person: " + result.PersonId;
+
+                        PersonStatusTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+                    }
+                }
+            }
+        }
+
+
+        private async void CreateFolderButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (personName.Length > 0 && personId != Guid.Empty)
+            {
+                StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
+                personFolder = await picturesFolder.CreateFolderAsync(personName, CreationCollisionOption.OpenIfExists);
+                await Launcher.LaunchFolderAsync(personFolder);
+            }
+        }
+
+
+
+        private async void SubmitToAzureButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            int imageCounter = 0;
+            var items = await personFolder.GetFilesAsync();
+            List<StorageFile> imageFilesToUpload = new List<StorageFile>();
+            foreach (StorageFile item in items)
+            {
+                //Windows Cam default save type is jpg
+                if (item.FileType == ".jpg")
+                {
+                    imageCounter++;
+                    imageFilesToUpload.Add(item);
+                }
+            }
+
+            if (imageCounter >= minPhotos)
+            {
+                imageCounter = 0;
+                try
+                {
+                    foreach (StorageFile imageFile in imageFilesToUpload)
+                    {
+                        imageCounter++;
+                        using (Stream s = await imageFile.OpenStreamForReadAsync())
+                        {
+                            AddPersistedFaceResult addResult = await faceServiceClient.AddPersonFaceAsync(personGroupId, personId, s);
+                            Debug.WriteLine("Add result: " + addResult + addResult.PersistedFaceId);
+
+                        }
+                        SubmissionStatusTextBlock.Text = "Submitted Image n. " + imageCounter;
+                    }
+                    SubmissionStatusTextBlock.Text = "Total Images submitted: " + imageCounter;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Submission Exc: " + ex.Message);
+                }
+            }
+            else
+            {
+                SubmissionStatusTextBlock.Text = $"Please add at least {minPhotos} face images to the person folder.";
+            }
+        }
+
+        private async void TrainButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (personGroupId.Length > 0)
+            {
+                await faceServiceClient.TrainPersonGroupAsync(personGroupId);
+
+                TrainingStatus trainingStatus = null;
+                while (true)
+                {
+                    trainingStatus = await faceServiceClient.GetPersonGroupTrainingStatusAsync(personGroupId);
+
+                    if (trainingStatus.Status != Status.Running)
+                    {
+                        break;
+                    }
+                    await Task.Delay(1000);
+                }
+
+                TrainStatusTextBlock.Text = "Training Completed!";
+            }
+        }
+
+        private async void DeletePersonButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            personName = PersonNameTextBox.Text;
+            bool personExist = false;
+            Person[] ppl = await GetKnownPeople();
+            foreach (Person p in ppl)
+            {
+                if (p.Name == personName)
+                {
+                    personExist = true;
+                    PersonStatusTextBlock.Text = $"Deleting person: {p.Name} ID: {p.PersonId}";
+                    await RemovePerson(p);
+                }
+            }
+            if (!personExist)
+            {
+                PersonStatusTextBlock.Text = $"No persons found to delete.";
+            }
+
+        }
+
+        internal async Task<Person[]> GetKnownPeople()
+        {
+            Person[] people = null;
+            if (null != faceServiceClient)
+            {
+                people = await faceServiceClient.ListPersonsAsync(personGroupId);
+            }
+            return people;
+        }
+
+        internal async Task RemovePerson(Person person)
+        {
+            if (null != person)
+            {
+                await faceServiceClient.DeletePersonAsync(personGroupId, person.PersonId);
+            }
+        }
+    }
+}
