41d12d8e0cc8635317d94b128e1c3f4e439debb1
fred le <flecoqui@hotmail.com>
2018-02-20 08:00:58 +0100

    Add UWP App in Lab5 Holographic and Immersive devices

--- Lab5/MR_AssetBundles_Loader/Assets/Scripts/AssetBundleManager.cs
+++ Lab5/MR_AssetBundles_Loader/Assets/Scripts/AssetBundleManager.cs
@@ -116,12 +116,13 @@ namespace AssetBundles
 	
 		private static string GetStreamingAssetsPath()
 		{
-			if (Application.isEditor)
-				return "file://" +  System.Environment.CurrentDirectory.Replace("\\", "/"); // Use the build output folder directly.
+			//if (Application.isEditor)
+			//	return "file://" +  System.Environment.CurrentDirectory.Replace("\\", "/"); // Use the build output folder directly.
             //flecoqui
 			//else if (Application.isWebPlayer)
 			//	return System.IO.Path.GetDirectoryName(Application.absoluteURL).Replace("\\", "/")+ "/StreamingAssets";
-			else if (Application.isMobilePlatform || Application.isConsolePlatform)
+			//else 
+            if (Application.isMobilePlatform || Application.isConsolePlatform)
 				return Application.streamingAssetsPath;
 			else // For standalone player.
 				return "file://" +  Application.streamingAssetsPath;
