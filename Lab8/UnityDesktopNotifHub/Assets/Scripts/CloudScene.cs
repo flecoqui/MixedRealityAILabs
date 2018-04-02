732ddd0ea83480ec3f8351d879118f792c9fd4ba
Frédéric Le Coquil <flecoqui@microsoft.com>
2018-04-02 13:05:20 +0200

    Add Lab8 related to notification hub

---
+++ Lab8/UnityDesktopNotifHub/Assets/Scripts/CloudScene.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CloudScene : MonoBehaviour {
+    /// <summary> 
+    /// Allows this class to behave like a singleton 
+    /// </summary> 
+    public static CloudScene instance;
+    /// <summary> 
+    /// Insert here you Azure Function Url 
+    /// </summary> 
+    private string azureFunctionEndpoint = "--Insert here you Azure Function Endpoint--";
+    /// <summary> 
+    /// Flag for object being moved 
+    /// </summary> 
+    private bool gameObjHasMoved;
+    /// <summary> 
+    /// Transform of the object being dragged by the mouse 
+    /// </summary> 
+    private Transform gameObjHeld;
+    /// <summary> 
+    /// Class hosted in the TableToScene script 
+    /// </summary> 
+    AzureTableEntity azureTableEntity;
+    /// <summary> 
+    /// Triggers before initialization 
+    /// </summary> 
+    void Awake()
+    {
+        // static instance of this class 
+        instance = this;
+    }
+
+    /// <summary> 
+    /// Use this for initialization 
+    /// </summary> 
+    void Start()
+    {
+        // initialise an AzureTableEntity 
+        azureTableEntity = new AzureTableEntity();
+    }
+
+    /// <summary> 
+    /// Update is called once per frame 
+    /// </summary> 
+    void Update()
+    {
+        //Enable Drag if button is held down 
+        if (Input.GetMouseButton(0))
+        {
+            // Get the mouse position 
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Raycast from the current mouse position to the object overlapped by the mouse 
+            if (Physics.Raycast(ray, out hit))
+            {
+                // update the position of the object "hit" by the mouse 
+                hit.transform.position = objPos;
+                gameObjHasMoved = true;
+                gameObjHeld = hit.transform;
+            }
+        }
+        // check if the left button mouse is released while holding an object 
+        if (Input.GetMouseButtonUp(0) && gameObjHasMoved)
+        {
+            gameObjHasMoved = false;
+            // Call the Azure Function that will update the appropriate Entity in the Azure Table 
+            // and send a Notification to all subrscribed Apps 
+            Debug.Log("Calling Azure Function");
+            StartCoroutine(UpdateCloudScene(gameObjHeld.name,
+                      gameObjHeld.position.x, gameObjHeld.position.y, gameObjHeld.position.z));
+        }
+    }
+
+    private IEnumerator UpdateCloudScene(string objName, double xPos, double yPos, double zPos)
+    {
+        WWWForm form = new WWWForm();
+        // set the properties of the AzureTableEntity 
+        azureTableEntity.RowKey = objName;
+        azureTableEntity.X = xPos;
+        azureTableEntity.Y = yPos;
+        azureTableEntity.Z = zPos;
+        // Serialise the AzureTableEntity object to be sent to Azure 
+        string jsonObject = JsonConvert.SerializeObject(azureTableEntity);
+        using (UnityWebRequest www = UnityWebRequest.Post(azureFunctionEndpoint, jsonObject))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonObject);
+            www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            www.uploadHandler.contentType = "application/json";
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.SendWebRequest();
+            string response = www.responseCode.ToString();
+        }
+
+    }
+}
---
+++ Lab8/UnityDesktopNotifHub/Assets/Scripts/TableToScene.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using UnityEngine;
+
+
+/// <summary> 
+/// This objects is used to serialise and deserialise the Azure Table Entity 
+/// </summary> 
+[System.Serializable]
+public class AzureTableEntity : TableEntity
+{
+    public AzureTableEntity(string partitionKey, string rowKey)
+        : base(partitionKey, rowKey) { }
+
+
+    public AzureTableEntity() { }
+
+
+    public string Type { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+}
+
+public class TableToScene : MonoBehaviour {
+    /// <summary>          
+    /// allows this class to behave like a singleton 
+    /// </summary>          
+    public static TableToScene instance; 
+
+    /// <summary>          
+    /// Insert here you Azure Storage name
+    /// </summary>          
+    private string accountName = " -- Insert your Azure Storage name -- "; 
+
+    /// <summary>          
+    /// Insert here you Azure Storage key          
+    /// </summary>          
+    private string accountKey = " -- Insert your Azure Storage key -- ";
+    /// <summary> 
+    /// Triggers before initialization 
+    /// </summary> 
+    void Awake()
+    {
+        // static instance of this class 
+        instance = this;
+    }
+    /// <summary> 
+    /// Use this for initialization 
+    /// </summary> 
+    void Start()
+    {
+        //call method to populate the scene with new objects as  
+        //    specified in the Azure Table 
+        PopulateSceneFromTableAsync();
+    }
+
+     /// <summary>          
+     /// Populate the scene with new objects as specified in the Azure Table        
+     /// </summary> 
+     private async void PopulateSceneFromTableAsync() 
+     { 
+         // Obtain credentials for the Azure Storage 
+         StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+        // Storage account 
+        CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);                  // Storage client 
+        CloudTableClient client = account.CreateCloudTableClient();                   // Table reference 
+        CloudTable table = client.GetTableReference("SceneObjectsTable");
+        TableContinuationToken token = null; 
+         // Query the table for every existing Entity 
+         do 
+         { 
+             // Queries the whole table by breaking it into segments 
+             // (would happen only if the table had huge number of Entities) 
+             TableQuerySegment<AzureTableEntity> queryResult =
+                                  await table.ExecuteQuerySegmentedAsync(
+                                  new TableQuery<AzureTableEntity>(), token);  
+             foreach (AzureTableEntity entity in queryResult.Results) 
+             { 
+                 GameObject newSceneGameObject = null; 
+                 // check for the Entity Type and spawn in the scene the appropriate Primitive 
+                 switch (entity.Type) 
+                 { 
+                     case "Cube": 
+                         // Create a Cube in the scene 
+                         newSceneGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube); 
+                         break; 
+                     case "Sphere": 
+                         // Create a Sphere in the scene 
+                         newSceneGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+                         break; 
+                     case "Cylinder": 
+                         // Create a Cylinder in the scene 
+                         newSceneGameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder); 
+                         break; 
+                 }
+newSceneGameObject.name = entity.RowKey; 
+                 //check for the Entity X,Y,Z and move the Primitive at those coordinates 
+                 newSceneGameObject.transform.position =  
+                              new Vector3((float) entity.X, (float) entity.Y, (float) entity.Z); 
+             } 
+             // if the token is null, it means there are no more segments left to query 
+             token = queryResult.ContinuationToken; 
+         } 
+         while (token != null); 
+     } 
+}
